3239d06acd0f9462b87821b7f67a5575b8e501be
stomaras <sptoma724@gmail.com>
2020-05-06 17:44:49 +0300

    push

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome'
+import Hello from './components/Hello'
+import Message from './components/Message'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Message />
+        {/*<Greet name="Bruce" heroName="Batman"><p>This is children </p></Greet>
+        <Greet name="Clark" heroName="Superman">
+          <button>Action</button>
+        </Greet>
+        <Greet name="Diana" heroName="Wonder Woman"/>
+        <Welcome name="Bruce" heroName="Batman"/>
+        <Welcome name="Clark" heroName="Superman"/>
+        <Welcome name="Diana" heroName="Wonder Woman"/>*/}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Message.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+class Message extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            message: 'Welcome Visitor'
+
+        }
+    }
+
+    changeMessage() {
+        this.setState({
+            message: 'Thank you for subscibing'
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.message}</h1>
+                <button onClick={() => this.changeMessage()}>Subscibe</button>
+            </div>
+            
+        )
+            
+    }
+}
+
+export default Message
