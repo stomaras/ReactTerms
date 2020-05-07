e30c94e4c01d72f874d547889a8625639cd4f1a9
stomaras <sptoma724@gmail.com>
2020-05-07 14:59:49 +0300

    Destructuring in class components

--- src/App.js
+++ src/App.js
@@ -19,8 +19,9 @@ class App extends Component {
         </Greet>
         <Welcome name="Bruce" heroName="Batman"/>
         <Welcome name="Clark" heroName="Superman"/>
-        <Welcome name="Diana" heroName="Wonder Woman"/>*/}
+        */}
         <Greet name="Diana" heroName="Wonder Woman"/>
+        <Welcome name="Diana" heroName="Wonder Woman"/>
       </div>
     );
   }
