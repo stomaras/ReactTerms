c869a313877abb2e4a079fec1420a08dd51ab9e2
stomaras <sptoma724@gmail.com>
2020-05-07 14:11:48 +0300

    seState Done

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from './components/Message'
+import Counter from './components/Counter'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Message />
+        <Counter />
+        {/*<Message />*/}
         {/*<Greet name="Bruce" heroName="Batman"><p>This is children </p></Greet>
         <Greet name="Clark" heroName="Superman">
           <button>Action</button>
