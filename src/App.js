fb973d24a2afc8f5846dd562f8a5be8334baefff
stomaras <sptoma724@gmail.com>
2020-05-07 16:27:52 +0300

    Binding Event Handlers

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import Message from './components/Message'
 import Counter from './components/Counter'
 import FunctionClick from './components/FunctionClick'
 import ClassClick from './components/ClassClick'
+import EventBind from './components/EventBind'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <FunctionClick />
-        <ClassClick />
+        <EventBind />
+        {/*<FunctionClick />*/}
+        {/*<ClassClick />*/}
         {/*<Counter />*/}
         {/*<Message />*/}
         {/*<Greet name="Bruce" heroName="Batman"><p>This is children </p></Greet>
