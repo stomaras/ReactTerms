e01390f7e160763792e69fa4d2ae10a2b0ab509d
stomaras <sptoma724@gmail.com>
2020-05-09 21:15:42 +0300

    Basics of Form Handling

--- src/App.js
+++ src/App.js
@@ -13,12 +13,14 @@ import ParentComponent from './components/ParentComponent'
 import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList'
 import Stylesheet from './components/Stylesheet'
+import Form from './components/Form'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Stylesheet primary={false} />
+        <Form />
+        {/*<Stylesheet primary={false} />*/}
         {/*<NameList />*/}
         {/*<UserGreeting />/*}
         {/*<ParentComponent />*/}
